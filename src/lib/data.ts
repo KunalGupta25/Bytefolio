6fb4e968458a87eb7243f736f8221d0b80af41c1
Kunal Gupta <kkunalggupta200@gmail.com>
2025-05-14 10:14:25 +0000

    updated favicon path

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -73,7 +73,7 @@ const DEFAULT_SITE_SETTINGS: SiteSettings = {
     github: 'https://github.com/yourusername',
     twitter: 'https://twitter.com/yourusername',
   },
-  faviconUrl: "/favicon.png", // Updated default
+  faviconUrl: "https://img.icons8.com/?size=512&id=114904&format=png", // Updated default
 };
 
 const DEFAULT_ABOUT_DATA: AboutData = {
